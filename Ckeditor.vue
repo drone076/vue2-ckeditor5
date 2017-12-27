ad3e47538eb6eda6d3a82416dc2fae2649472ea6
Drone <drone076@yahoo.com>
2017-12-27 16:33:03 +0300

    Ckeditor.vue

---
+++ Ckeditor.vue
@@ -0,0 +1,47 @@
+<template>
+    <textarea id="editor" :value="value"
+        v-on:input="updateValue($event.target.value)" >
+    </textarea>
+</template>
+
+<script>
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic'
+
+export default {
+  name: 'Ckeditor',
+  data: function () {
+    return {
+      instance: null
+    }
+  },
+  props: ['value'],
+  watch: {
+    value () {
+      let html = this.instance.getData()
+      if (html !== this.value) {
+        this.instance.setData(this.value)
+      }
+    }
+  },
+  mounted () {
+    ClassicEditor
+      .create(this.$el, {
+        toolbar: ['bold', 'italic', 'link', 'bulletedList']
+      })
+      .then(editor => {
+        this.instance = editor
+        editor.document.on('changesDone', (evt, data) => {
+          this.updateValue(editor.getData())
+        })
+      })
+      .catch(error => {
+        console.error(error)
+      })
+  },
+  methods: {
+    updateValue: function (value) {
+      this.$emit('input', value)
+    }
+  }
+}
+</script>
